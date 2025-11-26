47c7cca6a26c9094a83da79095fedc4e38453b2c
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2025-11-25 22:13:10 -0300

    Criando os model de Produto e organizando o projeto

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Sequelize } from 'sequelize';
 
 const HOST = '127.0.0.1'
 const PORT = '5000'
@@ -13,18 +12,6 @@ app.get('/', (req, res) => {
     res.send('API de Produtos funcionando 🚀');
 })
 
-const sequelize = new Sequelize(
-    'postgresql://neondb_owner:npg_7AViXEFWeg5U@ep-polished-smoke-ad3mfwae-pooler.c-2.us-east-1.aws.neon.tech/planteidb?sslmode=require&channel_binding=require',
-    {
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true
-            }
-        },
-        loggin: true
-    }
-) // Example for postgres
 
 try {
     await sequelize.authenticate()
