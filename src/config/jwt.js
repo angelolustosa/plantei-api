ac4d26554238e1cba0ce22611f701bd0d9c9510c
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2026-02-10 21:38:22 -0300

    Token expirar com 1 min para teste

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,5 +1,5 @@
 export default {
   // Chave secreta para assinar o token JWT em https://jwtsecrets.com/ (256 bits)
   secret: process.env.JWT_SECRET || '2bf66e6bf284d3c7c554022e61bbe3b02ab29de157ea3349f821960bfbb36728',
-  expiresIn: '1h',
+  expiresIn: '1m', // Tempo de expiração do token
 };
